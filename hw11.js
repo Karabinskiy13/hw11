11cd24eec0af4ad21d1b29072e71bf00a04d5a23
Karabinskiy13 <ivan.karabinskiy1308@gmail.com>
2021-12-20 13:54:47 +0200

    Initial commit

---
+++ hw11.js
@@ -0,0 +1,45 @@
+// -створити форму з інпутами для name та age.
+//     При відправці форми записати об'єкт в localstorage
+//
+
+// let form = document.createElement('form');
+// let name = document.createElement('input');
+// let age = document.createElement('input');
+// let send = document.createElement('button')
+// let user = {};
+//
+// send.innerText = 'Send';
+//
+// send.onclick = function () {
+//     user.name = name.value;
+//     user.age = age.value;
+//     localStorage.setItem('User', JSON.stringify(user))
+// }
+//
+// form.append(name,age)
+// document.body.append(form,send)
+//
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+
+
+// let carForm = document.createElement('form');
+// let inputModel = document.createElement('input')
+// let inputType = document.createElement('input')
+// let inputVolume = document.createElement('input')
+// let sendBtn = document.createElement('button')
+// let box = 'box';
+// sendBtn.innerText = 'Send';
+//
+// carForm.append(inputModel,inputType,inputVolume);
+// document.body.append(carForm,sendBtn)
+//
+// let saveCar = (model, type, volume) => {
+//     let newArr = JSON.parse(localStorage.getItem(box)) || [];
+//     newArr.push({model, type, volume})
+//     localStorage.setItem(box, JSON.stringify(newArr));
+// };
+// sendBtn.onclick = () => {
+//     saveCar(inputModel.value, inputType.value, inputVolume.value);
+// }
+
